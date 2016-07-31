2cb4c5fd11d58bec70b973e9419711287c871617
sailsxu <sailsxu@qq.com>
2016-07-31 13:51:06 +0800

    更改成自己的配置（helpshift）

--- Potatso/DefaultConfidential.h
+++ Potatso/DefaultConfidential.h
@@ -3,8 +3,8 @@
 #ifndef Confidential_h
 #define Confidential_h
 
-#define HELPSHIFT_KEY    @""
-#define HELPSHIFT_DOMAIN @""
-#define HELPSHIFT_ID     @""
+#define HELPSHIFT_KEY    @"d8c0430f1ffa9b86409462bd2e73c2a2"
+#define HELPSHIFT_DOMAIN @"theirfun.helpshift.com"
+#define HELPSHIFT_ID     @"theirfun_platform_20160731035131870-f8ff594b2da7eac"
 
 #endif
